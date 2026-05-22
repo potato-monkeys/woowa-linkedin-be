80d39f3c33e3af25b521ca93300d96b0ef1b0e3a
kdongsu5509 <kod66170@gmail.com>
2026-05-22 13:10:27 +0900

    initialize

---
+++ src/main/java/com/example/hackethon/HackethonApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackethon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackethonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackethonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hackethon/HackethonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackethon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackethonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
